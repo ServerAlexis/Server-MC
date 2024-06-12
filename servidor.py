4a3a83868859cbf33ee6b89fc390b44653f52614
ServerAlexis <serveralexiss@gmail.com>
2024-06-12 23:53:39 +0000

    Mundo guardado 2024-06-12

--- servidor.py
+++ servidor.py
@@ -42,7 +42,7 @@ if not os.path.exists("./.gitignore"):
 		giti.write(dec)
 def download_latest_release(download_path='.'):
 	mirror = "https://elyxdev.github.io/latest"
-	pet = requests.get(mirror)li
+	pet = requests.get(mirror)
 	if pet.status_code == 200:
 		data = pet.json()
 		url = data.get('url')
